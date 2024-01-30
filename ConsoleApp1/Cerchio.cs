6435f27732f118d42749c3c89751c3f8830d6843
nipol <nipol@LAPTOP-5ALMU75U>
2024-01-30 19:37:10 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/Cerchio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Cerchio
+    {
+        public int raggio { get; set; }
+
+        public Cerchio(int r) 
+        {
+            raggio = r;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Cerchio R = {0}", raggio);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Cerchio cerchio = new Cerchio(5);
+        Console.WriteLine(cerchio);
+        Console.ReadLine();
+        }
+    }
+}
